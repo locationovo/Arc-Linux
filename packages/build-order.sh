9c05200c4f293a94927e0f64c1d9f05cdc141801
locationovo <qaqwang11@gmail.com>
2026-07-12 19:02:58 +0800

    Update build-order.sh

--- packages/build-order.sh
+++ packages/build-order.sh
@@ -32,49 +32,49 @@ unpack() {
 # glibc
 unpack "glibc-2.43.tar.gz" "glibc-2.43" "tar.gz"
 cd "$BUILD_DIR/glibc-2.43" && mkdir -p build && cd build
-../configure --prefix=/usr --host=$HOST --disable-werror && make -j$JOBS && make install DESTDIR="$SYSROOT"
+../configure --prefix=/usr --host=$HOST --disable-werror && make -s -j$JOBS && make install DESTDIR="$SYSROOT"
 cd "$OLDPWD"
 
 # bash
 unpack "bash-5.3.tar.gz" "bash-5.3" "tar.gz"
 cd "$BUILD_DIR/bash-5.3"
-./configure --prefix=/usr --host=$HOST --enable-readline --without-bash-malloc && make -j$JOBS && make install DESTDIR="$SYSROOT"
+./configure --prefix=/usr --host=$HOST --enable-readline --without-bash-malloc && make -s -j$JOBS && make install DESTDIR="$SYSROOT"
 cd "$OLDPWD"
 
 # coreutils
 unpack "coreutils-9.11.tar.xz" "coreutils-9.11" "tar.xz"
 cd "$BUILD_DIR/coreutils-9.11"
-./configure --prefix=/usr --host=$HOST && make -j$JOBS && make install DESTDIR="$SYSROOT"
+./configure --prefix=/usr --host=$HOST && make -s -j$JOBS && make install DESTDIR="$SYSROOT"
 cd "$OLDPWD"
 
 # openssl
 unpack "openssl-4.0.1.tar.gz" "openssl-4.0.1" "tar.gz"
 cd "$BUILD_DIR/openssl-4.0.1"
-./Configure "$OPENSSL_TARGET" --prefix=/usr --cross-compile-prefix="$CROSS_COMPILE" no-shared && make -j$JOBS && make install DESTDIR="$SYSROOT"
+./Configure "$OPENSSL_TARGET" --prefix=/usr --cross-compile-prefix="$CROSS_COMPILE" no-shared && make -s -j$JOBS && make install DESTDIR="$SYSROOT"
 cd "$OLDPWD"
 
 # libarchive
 unpack "libarchive-3.8.8.tar.xz" "libarchive-3.8.8" "tar.xz"
 cd "$BUILD_DIR/libarchive-3.8.8"
-./configure --prefix=/usr --host=$HOST --disable-shared && make -j$JOBS && make install DESTDIR="$SYSROOT"
+./configure --prefix=/usr --host=$HOST --disable-shared && make -s -j$JOBS && make install DESTDIR="$SYSROOT"
 cd "$OLDPWD"
 
 # curl
-unpack "curl-8.21.0.tar.gz" "curl-8.21.0" "tar.gz"
+unpack "curl-8.21.0.tar.xz" "curl-8.21.0" "tar.xz"
 cd "$BUILD_DIR/curl-8.21.0"
-./configure --prefix=/usr --host=$HOST --with-openssl --disable-shared && make -j$JOBS && make install DESTDIR="$SYSROOT"
+./configure --prefix=/usr --host=$HOST --with-openssl --disable-shared && make -s -j$JOBS && make install DESTDIR="$SYSROOT"
 cd "$OLDPWD"
 
 # gpgme
 unpack "gpgme-2.1.2.tar.bz2" "gpgme-2.1.2" "tar.bz2"
 cd "$BUILD_DIR/gpgme-2.1.2"
-./configure --prefix=/usr --host=$HOST --disable-shared && make -j$JOBS && make install DESTDIR="$SYSROOT"
+./configure --prefix=/usr --host=$HOST --disable-shared && make -s -j$JOBS && make install DESTDIR="$SYSROOT"
 cd "$OLDPWD"
 
 # pacman
 unpack "pacman-7.1.0.tar.xz" "pacman-7.1.0" "tar.xz"
 cd "$BUILD_DIR/pacman-7.1.0"
-meson setup build --prefix=/usr -Dpkg-ext=.pkg.tar.zst && meson compile -C build && DESTDIR="$SYSROOT" meson install -C build
+meson setup build --buildtype plain --prefix=/usr -Dpkg-ext=.pkg.tar.zst && meson compile -C build && DESTDIR="$SYSROOT" meson install -C build
 cd "$OLDPWD"
 
 # util-linux
@@ -92,7 +92,7 @@ UTIL_HOST="${CROSS_COMPILE%-}"
     --without-ncurses \
     --without-tinfo
 
-make -j$JOBS
+make -s -j$JOBS
 make install DESTDIR="$SYSROOT"
 cd "$OLDPWD"
 
